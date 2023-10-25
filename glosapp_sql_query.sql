84b8b3c81e88169d4525627bc5a7f397630f6653
emmanueladdo <asememmanueladdo@gmail.com>
2023-10-25 15:55:45 +0000

    db glosapp

--- glosapp_sql_query.sql
+++ glosapp_sql_query.sql
@@ -9,3 +9,13 @@ CREATE TABLE `glosapp`.`products` (
   `uom_id` INT NOT NULL,
   `price_per_unit` DOUBLE NOT NULL,
   PRIMARY KEY (`product_id`));
+
+-- creates unit of messure  table 
+CREATE TABLE `glosapp`.`uom` (
+  `uom_id` INT NOT NULL AUTO_INCREMENT,
+  `uom_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`uom_id`));
+
+-- create the place holder uom 
+INSERT INTO `glosapp`.`uom` (`uom_id`, `uom_name`) VALUES ('1', 'each ');
+INSERT INTO `glosapp`.`uom` (`uom_id`, `uom_name`) VALUES ('2', 'kg');
