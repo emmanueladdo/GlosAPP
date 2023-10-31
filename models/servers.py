e77547a7463f048c77c0473d1b74fe242e92e336
IdiJr <idialijnr@gmail.com>
2023-10-31 15:02:10 +0000

    modified comments of in DB_connections.py and added functions for oders servers uom

---
+++ models/servers.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+from flask import Flask, request, jsonify
+import mysql.connector
+from DB_connections import get_sql_connection
+import json
+
+import products
+import orders
+import uom
+
+app = Flask(__name__)
+
+connection = get_sql_connection()
+
+
+@app.route('/getUOM', methods=['GET'])
+def get_uom():
+    """
+    Get all Units of Measurement (UOM) from the database.
+
+    Returns:
+        JSON response with UOM data.
+    """
+    response = uom.get_uoms(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/getProducts', methods=['GET'])
+def get_products():
+    """
+    Get all products from the database.
+
+    Returns:
+        JSON response with product data.
+    """
+    response = products.get_all_products(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/insertProduct', methods=['POST'])
+def insert_product():
+    """
+    Insert a new product into the database.
+
+    Returns:
+        JSON response with the product ID.
+    """
+    request_payload = json.loads(request.form['data'])
+    product_id = products.insert_new_product(connection, request_payload)
+    response = jsonify({'product_id': product_id})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/getAllOrders', methods=['GET'])
+def get_all_orders():
+    """
+    Get all orders from the database.
+
+    Returns:
+        JSON response with order data.
+    """
+    response = orders.get_all_orders(connection)
+    response = jsonify(response)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/insertOrder', methods=['POST'])
+def insert_order():
+    """
+    Insert a new order into the database.
+
+    Returns:
+        JSON response with the order ID.
+    """
+    request_payload = json.loads(request.form['data'])
+    order_id = orders.insert_order(connection, request_payload)
+    response = jsonify({'order_id': order_id})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/deleteProduct', methods=['POST'])
+def delete_product():
+    """
+    Delete a product from the database.
+
+    Returns:
+        JSON response with the product ID.
+    """
+    return_id = products.delete_product(connection, request.form['product_id'])
+    response = jsonify({'product_id': return_id})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Grocery Store Management System")
+    app.run(port=5000)
