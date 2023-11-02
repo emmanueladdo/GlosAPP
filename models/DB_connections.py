f9f1616ecc0b7e01dc2e861e420f666c7871f396
emmanueladdo <asememmanueladdo@gmail.com>
2023-11-02 04:28:41 +0000

    ddb connection

--- models/DB_connections.py
+++ models/DB_connections.py
@@ -11,5 +11,5 @@ __connection = None
 def get_sql_conn():
     global __connection
     if __connection is None:
-        __connection = connect(user="root", password="root", host="127.0.0.1", database="glosapp")
+        __connection = connect(user="root", password="1@werty.coM", host="127.0.0.1", database="glosapp", port="3305")
         return __connection
