814362a323c327b72fc1564813e528c19462c93f
IdiJr <idialijnr@gmail.com>
2023-11-09 13:31:25 +0000

    changed product_name to name to enable backend to add products to database

--- glosapp_sql_query.sql
+++ glosapp_sql_query.sql
@@ -5,7 +5,7 @@ CREATE SCHEMA IF NOT EXISTS `glosapp`;
 -- creates the product table and colums
 CREATE TABLE `glosapp`.`products` (
   `product_id` INT NOT NULL AUTO_INCREMENT,
-  `product_name` VARCHAR(100) NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
   `uom_id` INT NOT NULL,
   `price_per_unit` DOUBLE NOT NULL,
   PRIMARY KEY (`product_id`)
